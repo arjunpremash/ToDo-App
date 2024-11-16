16498d4408b53614161242b58f15a7a1b481d2d2
Arjun P Remash <72246854+arjun-p-remash@users.noreply.github.com>
2024-11-16 17:45:40 +0530

    UserDataAccess

---
+++ ToDo.Backend/ToDo.DataAccess.Contracts/IUserDataAccess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDo.Models;
+
+namespace ToDo.DataAccess.Contracts
+{
+    public interface IUserDataAccess
+    {
+        public Task CreateNewUserAsync (UserModel user);
+        public Task DeleteUserAsync (UserModel user);
+        public Task<UserModel> AuthenticateUserAsync (UserModel user);
+    }
+}
---
+++ ToDo.Backend/ToDo.DataAccess/UserDataAccess.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDo.DataAccess.Contracts;
+using ToDo.EntityFramework;
+using ToDo.EntityFramework.Entity;
+using ToDo.Models;
+
+namespace ToDo.DataAccess
+{
+    public class UserDataAccess : IUserDataAccess
+    {
+        private ApplicationDbContext _context;
+        private readonly IPasswordHasher<User> _passwordHasher;
+
+        public UserDataAccess(ApplicationDbContext context, IPasswordHasher<User> passwordHasher)
+        {
+            _context = context;
+            _passwordHasher = passwordHasher;
+        }
+
+        public async Task CreateNewUserAsync(UserModel userModel)
+        {
+            if (UserNameExists(userModel.Username))
+            {
+                throw new Exception("Username already exists");
+            }
+            User user = new User();
+            user.Username = userModel.Username;
+            user.PasswordHash = _passwordHasher.HashPassword(user, userModel.Password);
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<UserModel> AuthenticateUserAsync(UserModel userModel)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u=> u.Username == userModel.Username);
+            if (user == null) {
+                return null;
+            }
+            var result = _passwordHasher.VerifyHashedPassword(user, user.PasswordHash, userModel.Password);
+            return result == PasswordVerificationResult.Success ? userModel : null;
+        }
+
+        public Task DeleteUserAsync(UserModel user) { return Task.CompletedTask; }
+
+        private bool UserNameExists(string username)
+        {
+            if (_context.Users.Any(u => u.Username == username))
+                return true;
+            else
+                return false;
+        }
+
+    }
+}
