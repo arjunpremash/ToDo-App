e4f0f706c4781299b146dd16f416e69f9818687c
Arjun P Remash <72246854+arjun-p-remash@users.noreply.github.com>
2024-11-17 19:08:25 +0530

    task services

---
+++ ToDo.Frontend/src/app/Services/task.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  private baseApiUrl = 'https://localhost:7124/api/Todo';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getTasks(projectId: any){
+    return this.httpClient.get(`${this.baseApiUrl}?projectId=${projectId}`);
+  }
+}
